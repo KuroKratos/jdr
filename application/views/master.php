a81194f1abc23a5cb0e72d0620ce0fba4759b393
Julien BOISSON <j.boisson@thk.eu>
2017-11-20 16:31:28 +0100

    WIP - Game Master Panel

--- application/views/master.php
+++ application/views/master.php
@@ -45,18 +45,56 @@
         </div>
       </div>
 
+      <hr style="margin:0 0 20px 0">
+
       <!-- HP & PP, 1 PANEL/EACH -->
       <div class="row">
         <div class="col-xs-6">
-          <div class="panel panel-success">
+<!--          <div class="panel panel-success">
             <div class="panel-heading text-center" style="padding: 5px;">P.V.</div>
             <div class="panel-body text-center text-success" style="font-weight: bold; font-size: 18px; padding: 5px;"><?= $c['hp_cur'] ?> / <?= $c['hp_max'] ?></div>
+          </div>-->
+          <div class="progress">
+            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?= $c['hp_cur'] ?>"
+            aria-valuemin="0" aria-valuemax="<?= $c['hp_max'] ?>" style="width:<?= ($c['hp_cur']/$c['hp_max'])*100 ?>%">
+              <?= ($c['hp_cur'] . " / " . $c['hp_max']) ?>
+            </div>
           </div>
         </div>
         <div class="col-xs-6">
-          <div class="panel panel-info">
+<!--          <div class="panel panel-info">
             <div class="panel-heading text-center" style="padding: 5px;">P.P.</div>
             <div class="panel-body text-center text-info" style="font-weight: bold; font-size: 18px; padding: 5px;"><?= $c['pp_cur'] ?> / <?= $c['pp_max'] ?></div>
+          </div>-->
+          <div class="progress">
+            <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="<?= $c['pp_cur'] ?>"
+            aria-valuemin="0" aria-valuemax="<?= $c['pp_max'] ?>" style="width:<?= ($c['pp_cur']/$c['pp_max'])*100 ?>%">
+              <?= ($c['pp_cur'] . " / " . $c['pp_max']) ?>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr style="margin:0 0 20px 0">
+
+      <!-- COMPETENCES & ABILITIES BTN + Gold -->
+      <div class="row">
+        <div class="col-xs-4">
+          <div class="panel panel-default">
+            <div class="panel-heading text-center">Dons</div>
+            <div class="panel-body text-center" style="font-weight: bold; font-size: 18px; padding: 5px;"><a href="#">Voir</a></div>
+          </div>
+        </div>
+        <div class="col-xs-4">
+          <div class="panel panel-default">
+            <div class="panel-heading text-center">Compétences</div>
+            <div class="panel-body text-center" style="font-weight: bold; font-size: 18px; padding: 5px;"><a href="#">Voir</a></div>
+          </div>
+        </div>
+        <div class="col-xs-4">
+          <div class="panel panel-warning">
+            <div class="panel-heading text-center">Gold</div>
+            <div class="panel-body text-warning text-center" style="font-weight: bold; font-size: 18px; padding: 5px;"><?= $c['gold'] ?></div>
           </div>
         </div>
       </div>
